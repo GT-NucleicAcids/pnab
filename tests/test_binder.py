f512e3eb29e1a7739fef2284c9f6a8f65f6b280b
alenaizan <aalenaizan3@gatech.edu>
2019-05-20 14:25:06 -0400

    fix options in test_binder.py

--- tests/test_binder.py
+++ tests/test_binder.py
@@ -52,8 +52,8 @@ def test_binder_2():
 
     backbone = bind.Backbone()
     backbone.file_path = 'files/rna_bb.pdb'
-    backbone.interconnects = [9, 14]
-    backbone.linker = [12, 13]
+    backbone.interconnects = [10, 1]
+    backbone.linker = [13, 14]
 
     base = bind.Base()
     base.file_path = 'files/adenine.pdb'
@@ -66,19 +66,19 @@ def test_binder_2():
     hp = bind.HelicalParameters()
     hp.shift = 0
     hp.slide = 0
-    hp.twist = 32.73
-    hp.rise = 2.81
-    hp.inclination = 15.76
-    hp.tip = 7.35
-    hp.x_displacement = -4.61
-    hp.y_displacement = -0.19
+    hp.twist = 32.39
+    hp.rise = 2.53
+    hp.inclination = 22.9
+    hp.tip = 0.08
+    hp.x_displacement = -4.54
+    hp.y_displacement = -0.02
 
     rp = bind.RuntimeParameters()
     rp.num_steps = 1000000
     rp.type = 'GAFF'
     rp.algorithm = 'WMC'
     rp.energy_filter = [10000.0, 10000.0, 10000.0, 10000.0, 10000.0]
-    rp.max_distance = 0.15
+    rp.max_distance = 0.05
     rp.strand = ['Adenine']*10
     rp.is_double_stranded = False
 
