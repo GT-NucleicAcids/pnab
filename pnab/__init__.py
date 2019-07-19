64eda355c39444123cd492d21dba4066214d0a84
alenaizan <aalenaizan3@gatech.edu>
2019-07-19 16:28:35 -0400

    fix error

--- pnab/__init__.py
+++ pnab/__init__.py
@@ -6,5 +6,8 @@ from __future__ import division, absolute_import, print_function
 
 from . import bind
 from .driver.pNAB import pNAB
-from .driver.jupyter_widgets import builder
+try:
+    from .driver.jupyter_widgets import builder
+except:
+    pass
 from .extras import test
