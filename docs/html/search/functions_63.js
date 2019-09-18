5281c6cdb9b35b6327af95b6b2f355a67f269542
alenaizan <aalenaizan3@gatech.edu>
2019-09-18 15:50:36 -0400

    LGTM

--- docs/html/search/functions_63.js
+++ docs/html/search/functions_63.js
@@ -3,5 +3,5 @@ var searchData=
   ['calcrmsd',['calcRMSD',['../classPNAB_1_1ConformationSearch.html#ae1e9b2b4878232f0c03bd016377ec60a',1,'PNAB::ConformationSearch']]],
   ['center',['center',['../classPNAB_1_1Backbone.html#a35c7fde42b7575d81d1c16a4fae8688f',1,'PNAB::Backbone']]],
   ['chain',['Chain',['../classPNAB_1_1Chain.html#a64c64333f8965f218bf48e4de48482b4',1,'PNAB::Chain']]],
-  ['conformationsearch',['ConformationSearch',['../classPNAB_1_1ConformationSearch.html#a97bb7fea026df6e8f5df3e76b42517b7',1,'PNAB::ConformationSearch']]]
+  ['conformationsearch',['ConformationSearch',['../classPNAB_1_1ConformationSearch.html#a3cb02e472fb03cd19b67b723bf2943b5',1,'PNAB::ConformationSearch']]]
 ];
--- docs/html/search/functions_72.js
+++ docs/html/search/functions_72.js
@@ -4,7 +4,7 @@ var searchData=
   ['reportdata',['reportData',['../classPNAB_1_1ConformationSearch.html#a0093dc12fcca4543b08e5b0137a21fb8',1,'PNAB::ConformationSearch']]],
   ['rodrigues_5fformula',['rodrigues_formula',['../classPNAB_1_1HelicalParameters.html#ac2d059633abe0e9bbdcdbd52e9166139',1,'PNAB::HelicalParameters']]],
   ['rotate',['rotate',['../classPNAB_1_1Backbone.html#ac0f8dfd5ad1b4bf415841e1d504b8d0f',1,'PNAB::Backbone']]],
-  ['run',['run',['../classPNAB_1_1ConformationSearch.html#a75ab7fa537fc2e65664a8840edfdf83b',1,'PNAB::ConformationSearch::run()'],['../classpNAB_1_1pNAB.html#abbcaa408f31eac2d484ad9f342c5c77e',1,'pNAB.pNAB.run()'],['../namespacePNAB.html#a00219482cc91090ecf2ba21676860d3b',1,'PNAB::run()'],['../namespacejupyter__widgets.html#a563b8d98bb719f1b5c16548fbe543960',1,'jupyter_widgets.run()']]],
+  ['run',['run',['../classPNAB_1_1ConformationSearch.html#a75ab7fa537fc2e65664a8840edfdf83b',1,'PNAB::ConformationSearch::run()'],['../classpNAB_1_1pNAB.html#abbcaa408f31eac2d484ad9f342c5c77e',1,'pNAB.pNAB.run()'],['../namespacePNAB.html#aa87da292a72a4cae79aca4cf6df6c141',1,'PNAB::run()'],['../namespacejupyter__widgets.html#a563b8d98bb719f1b5c16548fbe543960',1,'jupyter_widgets.run()']]],
   ['runtime_5fparameters',['runtime_parameters',['../namespacejupyter__widgets.html#ad416615754e711ab6f1627ceb2c69efc',1,'jupyter_widgets']]],
   ['runtimeparameters',['RuntimeParameters',['../classPNAB_1_1RuntimeParameters.html#a896f5a24ccc61552db246c593bdd09a7',1,'PNAB::RuntimeParameters']]]
 ];
