2008c3bb1ed3e280c7707c3c96b2c40f1425c670
alenaizan <alenaizana@gmail.com>
2019-11-09 23:21:21 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ conda create -n pnab -c conda-forge python numpy cmake pkg-config openbabel eige
 
 source activate pnab
 
-cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -DBUILD_DOCS=ON
+cmake -Bbuild -DCMAKE_BUILD_TYPE=Release \
+              -DBUILD_DOCS=ON \
+              -DOPENBABEL3_INCLUDE_DIR=$CONDA_PREFIX/include/openbabel3 \
+              -DOPENBABEL3_LIBRARY=$CONDA_PREFIX/lib/libopenbabel.so \
+              -DOPENBABEL3_VERSION_MET=TRUE \
+              -DPYTHON_EXECUTABLE="$CONDA_PREFIX/bin/python"
 
 # build
 cd build
@@ -14,7 +19,7 @@ cp bind.*so ../pnab
 cd ..
 
 # install
-SP_DIR=$CONDA_PREFIX/lib/python3.7/site-packages
+SP_DIR=$CONDA_PREFIX/lib/python3.8/site-packages
 cp -R pnab ${SP_DIR}
 cp -R tests ${SP_DIR}/pnab
 cp build/bind.*.so ${SP_DIR}/pnab
